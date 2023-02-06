8f3a7d423ee95f04d05617797870757d3ad8779e
huong <huong@huong-Darter-Pro>
2023-02-06 09:17:29 +0700

    Update web

--- viindoo_academy/controllers/controllers.py
+++ viindoo_academy/controllers/controllers.py
@@ -2,9 +2,25 @@ from odoo import http
 class Academy(http.Controller):
     # có thể tạo 1 list các đường dẫn khác nhau cùng trỏ vào 1 page bằng việc cho vào list
     # phân quyền auth: 'public' | 'user' | 'admin' : với user hoặc user là admin thì phải login mới truy cập được
-    @http.route(['/viindoo_academy/academy/', '/viindoo/'], auth='public', website=True)
+    @http.route(['/academy/academy/', '/viindoo/'], auth='public', website=True)
     def index(self, **kw):
         Teachers=http.request.env['academy.teachers']
-        # .index là id của file template được định nghĩa muốn sử dụng
+        # .index là id của file template được định nghĩa muốn sử dụng ở module thay thế
         return http.request.render('viindoo_academy.index',{'teachers':Teachers.sudo().search([])
             })
+    
+    # @http.route('/academy/<name>/', auth='public', website=True)
+    # def teacher(self, name):
+    #     return '<h1>{}</h1>'.format(name)
+    
+    # @http.route('/academy/<int:id>/', auth='public', website=True)
+    # def teacher2(self, id):
+    #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+    #
+
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
+    def teacher3(self, teacher):
+        # academy.biography: là 1 template
+        return http.request.render('viindoo_academy.biography', {
+        'person': teacher
+    })
--- viindoo_academy/models/teacher.py
+++ viindoo_academy/models/teacher.py
@@ -1,6 +1,20 @@
 from odoo import models, fields, api
-
+from datetime import date
 class Teachers(models.Model):
     _name = 'academy.teachers'
+    _decription="Teachers"
 
     name = fields.Char()
+    birthday=fields.Date()
+    age = fields.Integer(
+        string='Age', 
+        compute='_compute_teacher_age', 
+        store=True
+        )
+    biography=fields.Html()
+    
+    #@api.depends('birthday')
+    def _compute_teacher_age(self):
+        today=date.today()
+        for r in self:
+            r.age = today.year-r.birthday.year 
